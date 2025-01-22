f447222fe10a622ada093c7d587ba331b9198303
victor Mameluk <viktormameluk@gmail.com>
2025-01-22 19:24:03 -0300

    cypress

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -22,11 +22,37 @@ describe('Cypress Playground', () => {
 
   it('types in the signature field, checks the checkbox to see the preview, the unchecks it', () =>{
     cy.get('#signature-textarea-with-checkbox').type('victor')
-
     cy.get('#signature-checkbox').check()
 
     cy.contains('#signature-triggered-by-check', 'victor').should('be.visible')
+    cy.get('#signature-checkbox').uncheck()
 
+    cy.contains('#signature-triggered-by-check', 'victor').should('not.exist')
+  })
+
+  it('checks both possible radios and asserts if it is "on" or "off" ', () => {
+    cy.contains('#on-off', 'ON').should('be.visible')
+    cy.get('#off').check()
+
+    cy.contains('#on-off', 'OFF').should('be.visible')
+    cy.contains('#on-off', 'ON').should('not.exist')
+
+    cy.get('#on').check()
+
+    cy.contains('#on-off', 'ON').should('be.visible')
+    cy.contains('#on-off', 'OFF').should('not.exist')
+
+  })
+
+  it('', () => {
+    cy.contains('p', "You haven't selected a type yet.").should('be.visible')// usado para saber se esta ok
+
+    cy.get('#selection-type').select('VIP')
+    cy.contains('#selection-type', 'VIP')
+    cy.get('#selection-type').select(1)
+  })
+
+  it.only('', () =>{
 
   })
 
