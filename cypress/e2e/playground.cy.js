0621b6dba4897611b90a596ed0a2e86c93d80abb
victor Mameluk <viktormameluk@gmail.com>
2025-01-27 20:08:47 -0300

    cypress exercises

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -3,7 +3,7 @@ describe('Cypress Playground', () => {
     cy.visit  ('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
   })
 
-  it('Mostre a pagina web', () => {
+  it('Visitando paginas web', () => {
     cy.get('#promotional-banner').should('be.visible')
     // implementação do caso de teste.
   })
@@ -30,7 +30,7 @@ describe('Cypress Playground', () => {
     cy.contains('#signature-triggered-by-check', 'victor').should('not.exist')
   })
 
-  it('Verificar se está "on" ou "off" ', () => {
+  it('Verificar o radio "on" ou "off" ', () => {
     cy.contains('#on-off', 'ON').should('be.visible')
     cy.get('#off').check()
 
@@ -44,7 +44,7 @@ describe('Cypress Playground', () => {
 
   })
 
-  it('', () => {
+  it('Selecionando opções em campos de seleção suspensa', () => {
     cy.contains('p', "You haven't selected a type yet.").should('be.visible')// usado para saber se esta ok
 
     cy.get('#selection-type').select('VIP')
@@ -52,7 +52,46 @@ describe('Cypress Playground', () => {
     cy.get('#selection-type').select(1)
   })
 
-  it('', () =>{
+  it('Selecionando múltiplas opções em campos do tipo select', () => {
+    cy.contains('p', "You haven't selected any fruit yet.").should('be.visible')
+
+    cy.get('#fruit').select(['apple', 'banana', 'cherry'])
+
+    cy.contains('p', "You've selected the following fruits: apple, banana, cherry").should('be.visible')
+  })
+
+  it('Testando upload de arquivos', () =>{
+    cy.get('input[type="file"]').selectFile('./cypress/fixtures/example.json')
+
+    cy.contains('p', 'The following file has been selected for upload: example.json').should('be.visible')
+  })
+
+  it('Interceptando e aguardando requisições que ocorrem a nível de rede', () => {
+    cy.intercept('GET', 'https://jsonplaceholder.typicode.com/todos/1').as('getTodo')
+
+    cy.contains('button', 'Get TODO').click()
+    cy.wait('@getTodo').its('response.statusCode').should('be.equal', 200)
+
+    cy.contains('li', 'TODO ID: 1').should('be.visible')
+    cy.contains('li', 'Title: delectus aut autem').should('be.visible')
+    cy.contains('li', 'Completed: false').should('be.visible')
+    cy.contains('li', 'User ID: 1').should('be.visible')
+  })
+
+  it.only('Sobrescrevendo o resultado de uma requisição á nivel de rede', () => {
+    const todo = require('../fixtures/todo')
+
+
+    cy.intercept('GET', 'https://jsonplaceholder.typicode.com/todos/1', {fixture: 'todo'}).as('getTodo')
+
+    cy.contains('button' , 'Get TODO').click()
+    cy.wait('@getTodo').its('response.statusCode').should('be.equal', 200)
+
+
+    cy.contains('li', `TODO ID: ${todo.id}`).should('be.visible')
+    cy.contains('li', `Title: ${todo.title}`).should('be.visible')
+    cy.contains('li', `Completed: ${todo.completed}`).should('be.visible')
+    cy.contains('li', `User ID: ${todo.userId}`).should('be.visible')
 
   })
 
